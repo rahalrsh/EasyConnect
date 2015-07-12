c795c5caa8b5f7f250118a22da06557450710b78
moataz elbarkouky <moatazelbarkouky@moatazs-MacBook-Pro.local>
2015-07-12 13:02:49 +0200

    contact info layout-database load test- twitter follow link-alert box

--- app/src/main/java/easyconnect/example/com/easyconnect/ContactInfoActivity.java
+++ app/src/main/java/easyconnect/example/com/easyconnect/ContactInfoActivity.java
@@ -1,22 +1,134 @@
 package easyconnect.example.com.easyconnect;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Button;
+import android.database.Cursor;
+import android.app.AlertDialog;
+import android.net.Uri;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class ContactInfoActivity extends Activity {
+public class ContactInfoActivity extends Activity implements OnClickListener{
+
+    TextView contact_name ;
+    TextView home_address ;
+    TextView email_address ;
+    TextView phone_number ;
+    ImageView profile_pic;
+    ImageView TwitterButton;
+    ImageView LinkedInButton;
+    ImageView FacebookButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_info);
+        //insert dummy values into the database for testing purposes
+        DatabaseInsertTest();
 
-        TextView textView = (TextView) findViewById(R.id.contactName);
-        String contactName = getIntent().getStringExtra("Contact Name");
-        textView.setText(contactName);
+
+        //get the data base values for this contact name assuming it is unique for now
+        String email = "n/a";
+        DBHandler MyDataBaseHandler = new  DBHandler(this);
+        MyDataBaseHandler = MyDataBaseHandler.open();
+        String query = "Select "+MyDataBaseHandler.EMAIL +" FROM "+ MyDataBaseHandler.TABLE_NAME +" WHERE "+MyDataBaseHandler.NAME+" LIKE 'evil'";
+        Cursor c = MyDataBaseHandler.db.rawQuery(query,null);
+        c.moveToFirst();
+
+            if(c.getString(c.getColumnIndex("email"))!= null){
+               email =  c.getString(c.getColumnIndex("email"));
+            }
+
+        MyDataBaseHandler.close();
+
+
+        // initialize all User Info
+        contact_name = (TextView) findViewById(R.id.contact_name);
+        contact_name.setText("DR.Evil");
+        home_address = (TextView) findViewById(R.id.home_address);
+        home_address.setText("h3e1m2");
+        email_address = (TextView) findViewById(R.id.email_address);
+        email_address.setText(email.toString());
+        phone_number = (TextView) findViewById(R.id.phone_number);
+        phone_number.setText("5142208630");
+        profile_pic = (ImageView)findViewById(R.id.profile_pic);
+        profile_pic.setImageResource(R.drawable.austin);
+
+        //Initialize links
+        FacebookButton = (ImageView)findViewById(R.id.facebook_button);
+        FacebookButton.setOnClickListener(this);
+
+
+        TwitterButton = (ImageView)findViewById(R.id.twitter_button);
+        TwitterButton.setOnClickListener(this);
+
+
+        LinkedInButton = (ImageView)findViewById(R.id.linkedin_button);
+        LinkedInButton.setOnClickListener(this);
+    }
+
+    public void DatabaseInsertTest()
+    {
+        DBHandler MyDataBaseHandler = new  DBHandler(this);
+        MyDataBaseHandler = MyDataBaseHandler.open();
+        MyDataBaseHandler.insertData("evil","drEvil@gmail.com");
+        MyDataBaseHandler.close();
+    }
+
+    public void onClick(View selected) {
+
+
+        AlertDialog message ;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.are_you_sure)
+                .setPositiveButton(R.string.fire, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // FIRE ZE MISSILES!
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://twitter.com/intent/user?screen_name=drevil"));
+                        startActivity(browserIntent);
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+
+
+        // Create the AlertDialog object and return it
+        message = builder.create();
+
+        switch (selected.getId()) {
+            case R.id.facebook_button: {
+            message.show();
+            }
+            case R.id.twitter_button: {
+                message.show();
+            }
+            case R.id.linkedin_button: {
+                message.show();
+            }
+            
+
+        }
 
     }
 
