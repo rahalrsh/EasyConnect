9e44109f83e0ab12a93aec6f0080135b0155e806
rahalrsh <rahalrsh@gmail.com>
2015-12-25 16:04:33 -0500

    Adding a menu for create add page

--- app/src/main/java/easyconnect/example/com/easyconnect/CreateAdActivity.java
+++ app/src/main/java/easyconnect/example/com/easyconnect/CreateAdActivity.java
@@ -38,7 +38,7 @@ public class CreateAdActivity extends AppCompatActivity implements View.OnClickL
         @Override
         public boolean onCreateOptionsMenu(Menu menu) {
             // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_confirm_info, menu);
+            getMenuInflater().inflate(R.menu.menu_create_ad, menu);
             return true;
         }
 
