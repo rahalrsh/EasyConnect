18eaffbb7d8796b82203a98cf72521a2a84db485
bkashyap <chokee.bk@gmail.com>
2015-12-30 23:43:55 -0500

    Added image upload to parse

--- app/src/main/java/easyconnect/example/com/easyconnect/MyApplication.java
+++ app/src/main/java/easyconnect/example/com/easyconnect/MyApplication.java
@@ -6,6 +6,10 @@ import android.content.pm.Signature;
 import android.util.Base64;
 import android.util.Log;
 
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseUser;
+
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
@@ -18,6 +22,17 @@ public class MyApplication extends Application{
     public void onCreate() {
         super.onCreate();
         printHashKey();
+        // Add your initialization code here
+        Parse.initialize(this, "ZFcKKaoiLqzeMdbEJXQjHVv7zIruhphnGRX6EePX", "1k6qhMReEU1iCDQNaPQDEHxLGlGQVZvgTVoOEBD8");
+
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        // If you would like all objects to be private by default, remove this
+        // line.
+        defaultACL.setPublicReadAccess(true);
+
+        ParseACL.setDefaultACL(defaultACL, true);
     }
 
     public void printHashKey(){
